2e68e6159fb82286016ce009971bdd6dd78bf1b7
borko81 <bstoilov81@gmail.com>
2022-05-14 12:47:39 +0300

    Add recursive example one

---
+++ recursion/file_one.py
@@ -0,0 +1,15 @@
+def factory(x: int) -> int:
+    """
+    Return factory recursion function
+    :param x: int
+    :return: int
+    """
+    if x == 1:
+        return 1
+    return x * factory(x - 1)
+
+
+if __name__ == '__main__':
+    search_num = 10
+    result = factory(search_num)
+    print(f"The factory of result {search_num} is {result}")
