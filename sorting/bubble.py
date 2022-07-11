e4b38ebf1a25fa17385704fb6893b14a313581d4
borko81 <bstoilov81@gmail.com>
2022-07-11 20:41:12 +0300

    Bubble sort with while

--- sorting/bubble.py
+++ sorting/bubble.py
@@ -6,7 +6,19 @@ def bubble_sort(elements: list) -> None:
                 elements[y], elements[y + 1] = elements[y + 1], elements[y]
 
 
+def bubble_sort_two(elements: list):
+    counter = 0
+    is_sorted = False
+    while not is_sorted:
+        is_sorted = True
+        for x in range(1, len(elements) - counter):
+            if elements[x] < elements[x - 1]:
+                elements[x], elements[x - 1] = elements[x - 1], elements[x]
+                is_sorted = False
+        counter += 1
+
+
 if __name__ == '__main__':
     arr_elements = [2, 1, 10, 23]
-    bubble_sort(arr_elements)
+    bubble_sort_two(arr_elements)
     print(", ".join([str(x) for x in arr_elements]))
